ca6a108390f83e448d47fcdaf708b59b980e52c2
oliylk <oshanthi.liyanage@ifs.com>
2026-04-26 14:57:59 +0530

    Initial commit for welcome

---
+++ welcome.py
@@ -0,0 +1,6 @@
+welcomeMessage ="Welcome to MIT program"
+print("\033[92m--------------------------------------")
+print("welcomeMessage.upper()")
+print("Faculty of Graduate Studies")
+print("Sabaragamuwa University")
+print("--------------------------------------\033[0m")
